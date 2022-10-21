34cbb3c438f5b971814c257f37c1a0d81a62971f
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-10-21 18:22:38 -0400

    feat: importa as models no index.ts

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -1,4 +1,8 @@
 import { Sequelize } from 'sequelize';
 import * as config from '../config/database';
+import Teams from './teamsModel';
+import Matches from './matchesModel';
+import Users from './usersModel';
 
 export default new Sequelize(config);
+export { Teams, Matches, Users };
