19de732fec795d057b52dce4b303795c0d2f7c61
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-10-21 16:51:51 -0400

    feat: cria Models de Matches

---
+++ app/backend/src/database/models/matchesModel.ts
@@ -0,0 +1,48 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+// import Teams from './teamsModel';
+
+class Matches extends Model {
+  public id!: number;
+  public homeTeam: number;
+  public homeTeamGoals: number;
+  public awayTeam: number;
+  public awayTeamGoals: number;
+  public inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+export default Matches;
