afd691926eff470d92ebc97f43f27e5f3dc2fd46
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-10-26 14:42:29 -0400

    fix: resolve erro de lint de dependência ciclica

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -1,8 +1,4 @@
 import { Sequelize } from 'sequelize';
 import * as config from '../config/database';
-import Teams from './teamsModel';
-import Matches from './matchesModel';
-import Users from './usersModel';
 
 export default new Sequelize(config);
-export { Teams, Matches, Users };
