0733ad8f51a007d0c8feb256c9e94eec9be7be84
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-10-26 20:37:01 -0400

    fix: protege corretamente palavra secreta no token

--- app/backend/src/database/helpers/auth.ts
+++ app/backend/src/database/helpers/auth.ts
@@ -1,7 +1,7 @@
 import jwt = require('jsonwebtoken');
 import { IUser } from '../interfaces';
 
-const secretWord = 'xablau';
+const secretWord = String(process.env.JWT_SECRET);
 
 const createToken = (userData: IUser) => {
   const token = jwt.sign({ userData }, secretWord, { algorithm: 'HS256', expiresIn: '1d' });
