8eb12786a4a13ebfef9c53132245af20bb5af8b5
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-11-07 20:12:58 -0400

    feat: cria camada controller para validação de token e retorna a role de user

--- app/backend/src/database/controllers/usersController.ts
+++ app/backend/src/database/controllers/usersController.ts
@@ -18,4 +18,15 @@ export default class UserController {
       return res.status(401).json({ message: 'Incorrect email or password' });
     }
   };
+
+  validateLogin = async (req: Request, res: Response) => {
+    const { authorization } = req.headers;
+    try {
+      const result = await this.userService.validateLogin(authorization as string);
+      return res.status(200).json(result);
+    } catch (error) {
+      console.log('Validate Login Error', error);
+      return res.status(500).json({ message: 'Error' });
+    }
+  };
 }
