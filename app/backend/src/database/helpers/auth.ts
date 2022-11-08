06364a30363d4b864592bd4ccbcb5f6c0c143cde
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-11-07 20:11:05 -0400

    feat: cria decodificação de token

--- app/backend/src/database/helpers/auth.ts
+++ app/backend/src/database/helpers/auth.ts
@@ -1,5 +1,6 @@
 import jwt = require('jsonwebtoken');
 import { IUser } from '../interfaces';
+import IVerifyToken from '../interfaces/resultVerifyToken';
 
 const secretWord = String(process.env.JWT_SECRET);
 
@@ -8,4 +9,12 @@ const createToken = (userData: IUser) => {
   return token;
 };
 
-export default { createToken };
+const verifyToken = (token: string) => {
+  let result: unknown;
+  jwt.verify(token, secretWord.trim(), (error, decoded) => {
+    result = { error, ...decoded };
+  });
+  return (result as IVerifyToken).userData;
+};
+
+export default { createToken, verifyToken };
