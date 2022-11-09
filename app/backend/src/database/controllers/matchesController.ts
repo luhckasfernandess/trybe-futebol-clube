1923e7d2eee87406162fea867531cf075476c25a
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-11-09 09:01:21 -0400

    feat: retorna busca por filtros

--- app/backend/src/database/controllers/matchesController.ts
+++ app/backend/src/database/controllers/matchesController.ts
@@ -1,4 +1,4 @@
-import { Response, Request } from 'express';
+import { Response, Request, NextFunction } from 'express';
 import MatchService from '../services/matchService';
 
 export default class MatchesController {
@@ -7,10 +7,21 @@ export default class MatchesController {
     this.matchService = new MatchService();
   }
 
-  getMatches = async (_req: Request, res: Response) => {
+  getMatches = async (req: Request, res: Response, next: NextFunction) => {
     try {
+      if (req.query.inProgress) return next();
       const matches = await this.matchService.getMatches();
       return res.status(200).json(matches);
+    } catch (error) {
+      return error;
+    }
+  };
+
+  getMatchesInProgress = async (req: Request, res: Response) => {
+    try {
+      const { inProgress } = req.query;
+      const matchesInProgress = await this.matchService.getMatchesInProgress(inProgress === 'true');
+      return res.status(200).json(matchesInProgress);
     } catch (error) {
       return error;
     }
