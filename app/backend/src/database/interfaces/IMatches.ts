a748b3d209f7d917778430fb788957264a4da72f
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-11-10 11:05:42 -0400

    feat: cria interface para partidas

---
+++ app/backend/src/database/interfaces/IMatches.ts
@@ -0,0 +1,10 @@
+interface IMatches {
+  id: number,
+  homeTeamGoals: number,
+  awayTeamGoals: number,
+  homeTeam: number,
+  awayTeam: number,
+  inProgress: boolean,
+}
+
+export default IMatches;
