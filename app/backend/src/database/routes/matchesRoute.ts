385d0a9100f54f95380db251541d49380c59f2d6
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-11-10 11:11:24 -0400

    feat: cria adiciona método POST na rota

--- app/backend/src/database/routes/matchesRoute.ts
+++ app/backend/src/database/routes/matchesRoute.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import MatchesController from '../controllers/matchesController';
+import Token from '../middlewares/tokenValidate';
 
 const matchesRoute = Router();
 const matchesController = new MatchesController();
+const token = new Token();
 
 matchesRoute.get('/', matchesController.getMatches, matchesController.getMatchesInProgress);
+matchesRoute.post('/', token.tokenValidate, matchesController.saveMatchesInProgress);
 
 export default matchesRoute;
