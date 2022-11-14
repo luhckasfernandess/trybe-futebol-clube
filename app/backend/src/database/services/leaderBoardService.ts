2ba1ed1cff1cfc74c2d65e4ffe2b5b22b870b0d4
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-11-14 10:42:27 -0400

    feat: cria função que imprime as infos do time da casa na tabela na camada service

---
+++ app/backend/src/database/services/leaderBoardService.ts
@@ -0,0 +1,24 @@
+import BusinessRuleCalc from '../helpers/businessRulesCalc';
+import IMatchesWithTeamName from '../interfaces/IMatchesWithTeamName';
+import MatchService from './matchService';
+import TeamService from './teamService';
+
+export default class LeaderBoardService {
+  private matchService: MatchService;
+  private teamService: TeamService;
+  constructor() {
+    this.matchService = new MatchService();
+    this.teamService = new TeamService();
+  }
+
+  leaderBoardHome = async () => {
+    const matches = await this.matchService.getMatches() as unknown;
+    const teams = await this.teamService.getAllTeams();
+    const result = await Promise.all(
+      teams.map(({ id, teamName }) =>
+        BusinessRuleCalc.homeTeamStatistics(id, teamName, matches as IMatchesWithTeamName[])),
+    );
+    result.sort(BusinessRuleCalc.sortByTotalPoints);
+    return result;
+  };
+}
