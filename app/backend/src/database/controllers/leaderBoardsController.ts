3f69122c99709b475890afdef9406b0e4bdf7349
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-11-14 10:44:07 -0400

    feat: imprime as infos correta na tela com a camada controller

---
+++ app/backend/src/database/controllers/leaderBoardsController.ts
@@ -0,0 +1,18 @@
+import { Response, Request } from 'express';
+import LeaderBoardService from '../services/leaderBoardService';
+
+export default class LeaderBoardsController {
+  private leaderBoardService: LeaderBoardService;
+  constructor() {
+    this.leaderBoardService = new LeaderBoardService();
+  }
+
+  leaderBoardHome = async (_req: Request, res: Response) => {
+    try {
+      const result = await this.leaderBoardService.leaderBoardHome();
+      return res.status(200).json(result);
+    } catch (error) {
+      return error;
+    }
+  };
+}
